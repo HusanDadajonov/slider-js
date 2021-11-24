124854c9124214c8cea39dafcd0851b56fdba8db
am1rcek <xusandadajonov4385@gmail.com>
2021-11-24 17:10:33 +0500

    awd

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function nextSlider(){
+    const elSliderItem = document.querySelectorAll("#slider__item");
+    for(let i = 0; i < elSliderItem.length - 1; i++){
+        if(elSliderItem[i].style.transform == "translateX(0px)"){
+            elSliderItem[i].style.transform = "translateX(1200px)";
+            break;
+        } 
+    }
+}
+
+function previousSlider(){
+    const elSliderItem = document.querySelectorAll("#slider__item");
+    for(let i = 0; i < elSliderItem.length; i++){
+        if(elSliderItem[i].style.transform == "translateX(1200px)"){
+            elSliderItem[i].style.transform = "translateX(0px)";
+            break;
+        } 
+    }
+}
