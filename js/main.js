8e99384c224affe881d34607b609fdbe9f83802e
am1rcek <xusandadajonov4385@gmail.com>
2021-11-24 18:26:11 +0500

    awd

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ function nextSlider(){
     const elSliderItem = document.querySelectorAll("#slider__item");
     for(let i = 0; i < elSliderItem.length - 1; i++){
         if(elSliderItem[i].style.transform == "translateX(0px)"){
-            elSliderItem[i].style.transform = "translateX(1200px)";
+            elSliderItem[i].style.transform = "translateX(100%)";
             break;
         } 
-    }
+        else if(elSliderItem[i - 1]){
+           console.log("awdaw");
+        }
+    } 
 }
 
 function previousSlider(){
     const elSliderItem = document.querySelectorAll("#slider__item");
     for(let i = elSliderItem.length - 1; i < elSliderItem.length; i--){
-        if(elSliderItem[i].style.transform == "translateX(1200px)"){
+        if(elSliderItem[i].style.transform == "translateX(100%)"){
             elSliderItem[i].style.transform = "translateX(0px)";
             break;
         } 
