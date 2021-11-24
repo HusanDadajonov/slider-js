5d46559d35700f7efa978eb3312b8338cd7952b5
am1rcek <xusandadajonov4385@gmail.com>
2021-11-24 17:40:18 +0500

    weadw

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function nextSlider(){
 
 function previousSlider(){
     const elSliderItem = document.querySelectorAll("#slider__item");
-    for(let i = 0; i < elSliderItem.length; i++){
+    for(let i = elSliderItem.length - 1; i < elSliderItem.length; i--){
         if(elSliderItem[i].style.transform == "translateX(1200px)"){
             elSliderItem[i].style.transform = "translateX(0px)";
             break;
